b216794a124313511b359364887153d54c1cbed7
Christine Treacy <christine927t@gmail.com>
2019-11-21 20:20:25 -0500

    hid design select theme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     $('#name').focus();
 });
 
-
+//hide 'other' input on load
 $('#other-title').hide();
 //click handler for 'other' job role option
-$('#title [value=other]').on('select', function(){
-    $('#other-title').show();
-})
-//hide 'other' input on load
+$('option[value="other"]').on('select', function(){
+     $('#other-title').show();
+ })
+
+
+//hide 'select theme' in design menu
+const $designSelect = $('#design option');
+console.log($designSelect);
+$designSelect.eq(0).hide();
+
+
+const $colorSelect = $('#color');
+console.log($colorSelect);
+$colorSelect.children.hide();
+
+// $(document).ready(function() {
+//     $("#colors").children('option').hide();
+
+// });
