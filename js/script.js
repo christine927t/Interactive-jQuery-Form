077c687b67a55ba20645290435e62421b754ffc5
Christine Treacy <christine927t@gmail.com>
2019-11-23 13:50:44 -0500

    changed default color dropdown when design is selected

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ $('#other-title').hide();
 const $jobSelect = $('#title');
 const $jobOption = $('#title option');
 $jobSelect.on('change', function(event){
-    //console.log($(event.target).val());
     if ($(event.target).val()==='other'){
     $('#other-title').show();
     } else {
@@ -17,15 +16,24 @@ $jobSelect.on('change', function(event){
 
 
 //hide 'select theme' in design menu
-const $designSelect = $('#design option');
-$designSelect.eq(0).hide();
+const $designSelect = $('#design');
+const $designSelectOptions = $('#design option');
+$designSelectOptions.eq(0).hide();
 
-
+//change default color dropdown option//hide all others
 const $colorSelect = $('#color');
-//console.log($colorSelect);
-//$colorSelect.children.hide();
+const $colorSelectOptions = $('#color option');
+const $colorPlaceholder = $("<option value='choosetheme' selected='selected'>Please select a T-shirt theme</option>");
+$colorSelect.prepend($colorPlaceholder);
+$colorSelect.children().hide();
 
-// $(document).ready(function() {
-//     $("#colors").children('option').hide();
+//change color dropdown options when design is chosen
+$designSelect.on('change',function(event){
+    if ($(event.target).val()==='js puns'){
+        $colorPlaceholder.remove();
+        $colorSelectOptions.eq(0).attr('selected','selected');
+        $('#color option:gt(0)') && $('#color option:lt(3)').show();
+        
+    }
 
-// });
+})
